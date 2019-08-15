6c0ec3013a2fde316f50465610c7e8fc90298cb1
Kirk Sullivan <kirk_135@hotmail.com>
2019-08-15 14:21:55 -0400

    Added the functionality to toggle the opening and closing of the modal

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import './App.css'
 import Modal from './Modal'
 
 class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <Modal />
-      </div>
-    )
+  state = {
+     modal: false
   }
-}
+
+  selectModal = (info) => {
+    this.setState({modal: !this.state.modal}) // true/false toggle
+  }
+
+  render() {
+     return (
+        <div className="App">
+           <p onClick={ this.selectModal }
+           >Open Modal</p>
+           <Modal
+               displayModal={this.state.modal}
+               closeModal={this.selectModal}
+           />
+        </div>
+     );
+}}
 
 export default App;
--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 const Modal = props => {
+
+  const divStyle = {
+    display: props.displayModal ? 'block' : 'none',
+  }
+
+  // const closeModal = (evt) => {
+  //   evt.stopPropagation();
+  //   props.closeModal()
+  // }
+
   return(
-    <div className="modal">
-      <div className='modal-content'>
-        <span className='close'>&times;</span>
+    <div className="modal"
+          onClick={() => props.closeModal()}
+          style={divStyle} >
+      <div className='modal-content'
+      onClick={ e => e.stopPropagation() } >
+        <span className='close'
+        onClick={() => props.closeModal() }>&times;</span>
       </div>
     </div>
   )
