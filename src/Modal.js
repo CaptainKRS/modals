0c524485fe1e1774d48ad527777cc00fd5055cc7
Kirk Sullivan <kirk_135@hotmail.com>
2019-08-15 14:02:23 -0400

    First commit with css and modal component added

---
+++ src/Modal.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Modal = props => {
+  return(
+    <div className="modal">
+      <div className='modal-content'>
+        <span className='close'>&times;</span>
+      </div>
+    </div>
+  )
+}
+
+export default Modal;
